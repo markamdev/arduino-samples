f9c5f37d4187860170e56bc6daa750fce59c6845
Marcin Kaminski <markamdev.84@gmail.com>
2020-04-16 22:33:47 +0200

    Brita timer finished
    Changes:
    - project simulating timer from brita filtering jug
    finished

--- brita_timer/brita_timer.ino
+++ brita_timer/brita_timer.ino
@@ -2,7 +2,7 @@
 #define LOWER_LED 9
 #define HIGHER_LED 12
 #define WARN_LED 7
-#define BUTTON_PIN 3
+#define BUTTON_PIN 2
 
 // additional safecheck (do not allow incorrect LED's settings)
 #if (HIGHER_LED <= LOWER_LED)
@@ -35,10 +35,14 @@ enum {
 };
 int new_leds_state = LEDS_EMPTY;
 int old_leds_state = LEDS_EMPTY;
-const long int LEDS_TIMER_DELAY = 10000;
-const long int LEDS_LOADING_DELAY = 1000;
-long int leds_timestamp = 0;
+const long int LEDS_TIMER_DELAY = 5000;
+const long int LEDS_LOADING_DELAY = 500;
+const long int LEDS_BLINKING_DELAY = 100;
+long int leds_loading_timestamp = 0;
+long int leds_timer_timestamp = 0;
+long int leds_blinking_timestamp = 0;
 int leds_level = 0;
+int leds_blinking_state = 0;
 
 // vars and consts for warning LED controler
 const long int WARN_BLINK_DELAY = 500;
@@ -50,22 +54,24 @@ long int current_time = 0;
 
 void setup() {
   Serial.begin(38400);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT);
   for (int i = LOWER_LED; i <= HIGHER_LED; i++)
   {
     pinMode(i, OUTPUT);
   }
   pinMode(WARN_LED, OUTPUT);
 
-  // some variables init
+  // some variables initialization
   warn_timestamp = millis();
-  leds_timestamp = warn_timestamp; // initialize with same value
+  leds_timer_timestamp = warn_timestamp; // initialize with same value
+  old_leds_state = new_leds_state = LEDS_EMPTY;
 }
 
 // functions forward declaration
 void process_button();
 void process_warning();
 void process_leds();
+void set_leds(unsigned int count);
 
 void loop() {
   current_time = millis();
@@ -82,7 +88,7 @@ inline void process_button()
 {
   // button debouncer code
   button_pin_state = digitalRead(BUTTON_PIN);
-  if (button_pin_state == HIGH)
+  if (button_pin_state == LOW)
   {
     if (button_state == BUTTON_PRESSED)
     {
@@ -124,30 +130,15 @@ inline void process_button()
   }
 }
 
-void set_leds(unsigned int count)
-{
-  if (count > LED_COUNT)
-  {
-    return;
-  }
-  // turn on X leds at beginning
-  int i  = 0;
-  for (i = 0; i < count; ++i)
-  {
-    digitalWrite(LOWER_LED + i, HIGH);
-  }
-  // turn off rest of leds
-  for (i = LOWER_LED + count; i < HIGHER_LED; ++i)
-  {
-    digitalWrite(i, LOW);
-  }
-}
-
 inline void process_leds()
 {
   if (button_state == BUTTON_PRESSED)
   {
-    new_leds_state = LEDS_LOADING;
+    if (new_leds_state != LEDS_LOADING && new_leds_state != LEDS_BLINKING)
+    {
+      new_leds_state = LEDS_LOADING;
+      leds_loading_timestamp = current_time;
+    }
   }
   else
   {
@@ -161,27 +152,74 @@ inline void process_leds()
     }
   }
 
-  if (new_leds_state != old_leds_state)
+  switch (new_leds_state)
   {
-    switch (new_leds_state)
-    {
-      case LEDS_EMPTY:
+    case LEDS_EMPTY:
+      if (old_leds_state != new_leds_state)
+      {
         set_leds(0);
-        break;
-      case LEDS_LOADING:
-        set_leds(2);
-        break;
-      case LEDS_TIMER:
-        break;
-      case LEDS_BLINKING:
-        break;
-      default:
-        // error condition
-        new_leds_state = LEDS_EMPTY;
-        Serial.println("* -> LEDS_EMPTY");
-    }
-    old_leds_state = new_leds_state;
+      }
+      break;
+    case LEDS_LOADING:
+      if ((current_time - leds_loading_timestamp) > LEDS_LOADING_DELAY)
+      {
+        if (leds_level < LED_COUNT)
+        {
+          // counter max not reached yed
+          leds_level++;
+          leds_loading_timestamp = current_time;
+          set_leds(leds_level);
+        }
+        else
+        {
+          new_leds_state = LEDS_BLINKING;
+          leds_blinking_timestamp = current_time;
+        }
+      }
+      break;
+    case LEDS_TIMER:
+      if (old_leds_state != LEDS_TIMER)
+      {
+        set_leds(leds_level);
+        leds_timer_timestamp = current_time;
+      }
+      if ((current_time - leds_timer_timestamp) > LEDS_TIMER_DELAY)
+      {
+        leds_level--;
+        if (leds_level == 0)
+        {
+          new_leds_state = LEDS_EMPTY;
+        }
+        else
+        {
+          leds_timer_timestamp = current_time;
+        }
+        set_leds(leds_level);
+      }
+      break;
+    case LEDS_BLINKING:
+      if ((current_time - leds_blinking_timestamp) > LEDS_BLINKING_DELAY)
+      {
+        if (leds_blinking_state == 0)
+        {
+          leds_blinking_state = 1;
+          set_leds(0);
+        }
+        else
+        {
+          leds_blinking_state = 0;
+          set_leds(LED_COUNT);
+        }
+        leds_blinking_timestamp = current_time;
+      }
+      break;
+    default:
+      // error condition
+      new_leds_state = LEDS_EMPTY;
+      Serial.println("* -> LEDS_EMPTY");
   }
+
+  old_leds_state = new_leds_state;
 }
 
 inline void process_warning()
@@ -205,5 +243,24 @@ inline void process_warning()
       // warn LED state changed - write to pin
       digitalWrite(WARN_LED, warn_state);
     }
+  }
+}
+
+void set_leds(unsigned int count)
+{
+  if (count > LED_COUNT)
+  {
+    return;
+  }
+  // turn on X leds at beginning
+  int i  = 0;
+  for (i = 0; i < count; ++i)
+  {
+    digitalWrite(LOWER_LED + i, HIGH);
+  }
+  // turn off rest of leds
+  for (i = LOWER_LED + count; i <= HIGHER_LED; ++i)
+  {
+    digitalWrite(i, LOW);
   }
 }
